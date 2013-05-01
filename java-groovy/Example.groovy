6eb8c4c3f2e5a1b9c7b0cebf01aec341c2d31868
David Madouros <dmadouros@comcast.net>
2013-05-01 09:19:37 -0600

    added notes to bottom of groovy file

--- java-groovy/Example.groovy
+++ java-groovy/Example.groovy
@@ -1,3 +1,16 @@
 new File("alpha.txt").eachLine {
   println it
 }
+
+/* 
+[ ] semi-colons
+[ ] imports
+[ ] Class
+[ ] psvm
+[ ] try-catch
+[ ] static types
+[ ] println
+[ ] eachLine (closure v. block)
+[ ] parens
+[ ] it
+*/
